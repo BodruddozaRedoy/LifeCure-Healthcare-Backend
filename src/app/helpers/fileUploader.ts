f7a26f1deb6c4918718f08d31c3332c31a5cd57f
bodruddozaredoy <bodruddozaredoy@gmail.com>
2025-10-09 04:21:20 +0600

    build(deps): update package-lock.json, package.json, fileUploader.ts and user.routes.ts

--- src/app/helpers/fileUploader.ts
+++ src/app/helpers/fileUploader.ts
@@ -1,8 +1,10 @@
 import multer from 'multer'
+import path from 'path'
+import {v2 as cloudinary} from 'cloudinary'
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, '/tmp/my-uploads')
+    cb(null, path.join(process.cwd(), "/uploads"))
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
@@ -11,3 +13,11 @@ const storage = multer.diskStorage({
 })
 
 const upload = multer({ storage: storage })
+
+const uploadToCloudinary = async (file: Express.Multer.File) => {
+    
+}
+
+export const fileUploader = {
+  upload
+}
--- src/app/modules/user/user.routes.ts
+++ src/app/modules/user/user.routes.ts
@@ -1,8 +1,9 @@
 import express from 'express'
 import { UserController } from './user.controller'
+import { fileUploader } from '../../helpers/fileUploader'
 
 const router = express.Router()
 
-router.post("/create-patient", UserController.createPatient)
+router.post("/create-patient", fileUploader.upload.single("file"), UserController.createPatient)
 
 export const userRoutes = router
