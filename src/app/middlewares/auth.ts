431dbcaa50d3526add306c5ebd641e5562e1f9d8
BodruddozaRedoy <bodruddozaredoy@gmail.com>
2025-10-28 15:42:24 +0600

    build(deps): update package-lock.json, package.json and auth.ts

--- src/app/middlewares/auth.ts
+++ src/app/middlewares/auth.ts
@@ -1,33 +1,39 @@
-import { NextFunction, Request, Response } from "express"
-import { jwtHelper } from "../helper/jwtHelper";
+import { NextFunction, Request, Response } from "express";
 import ApiError from "../errors/ApiError";
-import httpStatus from "http-status"
+import httpStatus from "http-status";
 import config from "../../config";
 import { Secret } from "jsonwebtoken";
+import { jwtHelper } from "../helpers/jwtHelper";
 
 const auth = (...roles: string[]) => {
-    return async (req: Request & { user?: any }, res: Response, next: NextFunction) => {
-        try {
-            const token = req.cookies.accessToken;
+  return async (
+    req: Request & { user?: any },
+    res: Response,
+    next: NextFunction
+  ) => {
+    try {
+      const token = req.cookies.accessToken;
 
-            if (!token) {
-                throw new ApiError(httpStatus.UNAUTHORIZED, "You are not authorized!")
-            }
+      if (!token) {
+        throw new ApiError(httpStatus.UNAUTHORIZED, "You are not authorized!");
+      }
 
-            const verifyUser = jwtHelper.verifyToken(token, config.jwt.jwt_secret as Secret);
+      const verifyUser = jwtHelper.verifyToken(
+        token,
+        config.jwt.jwt_secret as Secret
+      );
 
-            req.user = verifyUser;
+      req.user = verifyUser;
 
-            if (roles.length && !roles.includes(verifyUser.role)) {
-                throw new ApiError(httpStatus.UNAUTHORIZED, "You are not authorized!")
-            }
+      if (roles.length && !roles.includes(verifyUser.role)) {
+        throw new ApiError(httpStatus.UNAUTHORIZED, "You are not authorized!");
+      }
 
-            next();
-        }
-        catch (err) {
-            next(err)
-        }
+      next();
+    } catch (err) {
+      next(err);
     }
-}
+  };
+};
 
 export default auth;
